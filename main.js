4c865ccc2900f78fea5de5b6c0e72f165f5e0f69
Antoine <antoinejuillard015@gmail.com>
2023-04-15 20:04:41 -0500

    Nav burger

---
+++ main.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myFunction1() {
+    var  y= document.getElementById("Navigation");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+  }
